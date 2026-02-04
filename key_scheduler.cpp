acb874f0ee8f8e72c2c680f5e4dcf12a03ff7ee4
AddiThurston <addithurston@gmail.com>
2026-02-03 23:28:32 -0600

    added left cyclic shift

--- key_scheduler.cpp
+++ key_scheduler.cpp
@@ -7,13 +7,15 @@ using namespace std;
 
 string shift_left(string sub_key){
     // write code from here
-    
+    sub_key = sub_key.substr(1) + sub_key[0];   // shifting left is simple
     return sub_key;
-} 
+}
 
 vector<string> key_gen(string key){
     vector<string> sub_keys;
     //write code from here
-    
+    for (int i = 0; i < 16; i++) {
+        
+    }
     return sub_keys;
 }
