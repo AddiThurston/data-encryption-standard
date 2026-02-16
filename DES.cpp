efcc894552925e6ad50567d3acdf2577a649f564
AddiThurston <addithurston@gmail.com>
2026-02-16 15:05:21 -0600

    added encryption

--- DES.cpp
+++ DES.cpp
@@ -1,6 +1,5 @@
 #include "DES.h"
 #include "permutation.h"
-#include "des_utils.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -13,15 +12,25 @@ string encryption(string plaintext, vector<string> keys) {
     }
     string l = initialPermutation.substr(0,32);
     string r = initialPermutation.substr(32);
+
     for (int round = 0; round < 16; round++) {
-        string expanded(48, ' ');
-        for (size_t i = 0; i < 48; i++) {
-            expanded[i] = r[E[i]-1];
-        }
-        expanded = XOR(expanded, keys[round], 48);
-        l = r;
+        string tempL = l;   // store the initial left half to use it in xor later
+        l = r;  // the left half becomes the old right half
+        // then, we just do the order of operations in the chart
+        r = expansion(r);
+        r = XOR(r, keys[round], 48);
+        r = sBox(r);
+        r = pBox(r);
+        r = XOR(r, tempL, 32);
     }
-    return l+r;
+
+    // permute the final combo of r+l
+    string combo = r+l;
+    string finalPermutation(64, ' ');
+    for (size_t i = 0; i < 64; i++) {
+        finalPermutation[i] = combo[FP[i]-1];
+    }
+    return finalPermutation;
 }
 
 string decryption(string encrypted, vector<string> keys) {
